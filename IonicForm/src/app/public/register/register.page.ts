27dfc58bc4184ebf5e4d3c9cb7d3e72860dc30a4
jbaumli <jbaumli@mvl.kmmfg.com>
2019-10-22 13:18:42 -0500

    Updating theme

--- IonicForm/src/app/public/register/register.page.ts
+++ IonicForm/src/app/public/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; //**Added Line */
 import { Router } from  "@angular/router"; //**Added Line */
 import { AuthenticationService } from './../../services/authentication.service'; //**Added Line */
 @Component({
@@ -7,15 +8,31 @@ import { AuthenticationService } from './../../services/authentication.service';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(private  authService:  AuthenticationService, private  router:  Router) { } //**Modified Line */
-
+  constructor(private  authService:  AuthenticationService, private  router:  Router, private formBuilder: FormBuilder) { } //**Modified Line */
+  public onRegisterForm: FormGroup; //**Added Line */
   ngOnInit() {
+    //**Added Section - Start*/
+    this.onRegisterForm = this.formBuilder.group({
+      'name': [null, Validators.compose([
+        Validators.required
+      ])],
+      'email': [null, Validators.compose([
+        Validators.required
+      ])],
+      'password': [null, Validators.compose([
+        Validators.required
+      ])]
+    });
+    //**Added Section - End */
   }
   //**Added Section - Start*/
-  register(form) {
-    this.authService.register(form.value).subscribe((res) => {
+  register(onRegisterForm) {
+    this.authService.register(onRegisterForm.value).subscribe((res) => {
       this.router.navigateByUrl('home');
     });
+  }
+  goToLogin() {
+    this.router.navigateByUrl('login');
   }
   //**Added Section - End */
 }
